96428c81c22967a854d26c4de0c59771d43b06ee
chuchunf <chuchunf@users.noreply.github.com>
2021-07-17 19:35:10 +0800

    add first non-unique character in a string

---
+++ src/main/scala/hashmap/FirstUniqChar.scala
@@ -0,0 +1,13 @@
+package hashmap
+
+object FirstUniqChar {
+  def firstUniqChar(s: String): Int =
+    import scala.collection.mutable
+    val cache = mutable.Map[Char, Int]()
+
+    s.toArray.foreach(c => cache.update(c, cache.get(c).getOrElse(0) + 1))
+    println(cache)
+    s.toArray.find(cache.get(_).getOrElse(0) == 1) match
+      case Some(result) => s.indexOf(result)
+      case _ => throw new IllegalArgumentException("no character is unique in the given string")
+}
---
+++ src/test/scala/hashmap/FirstUniqCharTest.scala
@@ -0,0 +1,25 @@
+package hashmap
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+class FirstUniqCharTest extends AnyFunSuite {
+  test("can find the 1st unqiue character") {
+    val data = "leetcode"
+    val result = FirstUniqChar.firstUniqChar(data)
+    result shouldEqual (0)
+  }
+
+  test("can find the 1st unique character 2") {
+    val data = "leetcodel"
+    val result = FirstUniqChar.firstUniqChar(data)
+    result shouldEqual (3)
+  }
+
+  test("can throw exception if there is no unique chacter") {
+    val data = "aabb"
+    assertThrows[IllegalArgumentException] {
+      FirstUniqChar.firstUniqChar(data)
+    }
+  }
+}
