63efbcf9729d7d1690ba95bf7411a8ee9b8bf40c
chuchunf <chuchunf@users.noreply.github.com>
2021-07-31 11:18:20 +0800

    add invert binary tree

---
+++ src/test/scala/tree/InvertBinaryTreeTest.scala
@@ -0,0 +1,24 @@
+package tree
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+class InvertBinaryTreeTest extends AnyFunSuite {
+  test("can invert a single node") {
+    val root = TreeNode(1)
+    val result = InvertBinaryTree.invertTree(root)
+    result.value shouldEqual (1)
+    result.left shouldEqual (None)
+    result.right shouldEqual (None)
+  }
+
+  test("can invert a tree") {
+    val root = TreeNode(1)
+    root.left = Option(TreeNode(2))
+    root.right = Option(TreeNode(3))
+    val result = InvertBinaryTree.invertTree(root)
+    root.value shouldEqual (1)
+    root.left.get.value shouldEqual (3)
+    root.right.get.value shouldEqual (2)
+  }
+}
