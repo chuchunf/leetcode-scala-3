395bd764a637d905747b87b37ba0ddbc63ee604b
chuchunf <chuchunf@users.noreply.github.com>
2021-08-15 22:21:52 +0800

    add unit testing cases for BestTimetoBuyandSellStock

--- src/main/scala/array/BestTimetoBuyandSellStock.scala
+++ src/main/scala/array/BestTimetoBuyandSellStock.scala
@@ -2,7 +2,7 @@ package array
 
 object BestTimetoBuyandSellStock {
   def maxProfit(prices: Array[Int]): Int = prices match
-    case Array(_) => 0
+    case Array() => 0
     case Array(head, tail) => if head < tail then tail - head else 0
     case _ => var (maxprofit, minprice) = (0, prices(0))
       prices.drop(1).foreach(price => {
--- src/test/scala/array/BestTimetoBuyandSellStockTest.scala
+++ src/test/scala/array/BestTimetoBuyandSellStockTest.scala
@@ -1,7 +1,7 @@
 package array
 
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.*
 
 class BestTimetoBuyandSellStockTest extends AnyFunSuite {
   test("can find the max profit") {
@@ -11,8 +11,24 @@ class BestTimetoBuyandSellStockTest extends AnyFunSuite {
   }
 
   test("can return 0 if not profitable") {
-    val prices = Array(7,6,4,3,1)
+    val prices = Array(7, 6, 4, 3, 1)
     val result = BestTimetoBuyandSellStock.maxProfit(prices)
     result shouldEqual (0)
+  }
+
+  test("can return 0 with empty array") {
+    val prices = Array.empty[Int]
+    val result = BestTimetoBuyandSellStock.maxProfit(prices)
+    result shouldEqual (0)
+  }
+
+  test("can handle two element array") {
+    val prices = Array(7, 6)
+    val result = BestTimetoBuyandSellStock.maxProfit(prices)
+    result shouldEqual (0)
+
+    val prices2 = Array(7, 8)
+    val result2 = BestTimetoBuyandSellStock.maxProfit(prices2)
+    result2 shouldEqual (1)
   }
 }
