8636af2f1ecb01fe438867eab44c0e064d0a28df
chuchunf <chuchunf@users.noreply.github.com>
2021-08-15 22:07:47 +0800

    add unit testing cases for trap water and online stock span

--- src/test/scala/stack/OnlineStockSpanTest.scala
+++ src/test/scala/stack/OnlineStockSpanTest.scala
@@ -1,7 +1,7 @@
 package stack
 
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.*
 
 class OnlineStockSpanTest extends AnyFunSuite {
   test("can calculate span with price high to low") {
@@ -22,5 +22,17 @@ class OnlineStockSpanTest extends AnyFunSuite {
     result(2) shouldEqual (1)
     result(3) shouldEqual (3)
     result(4) shouldEqual (1)
+  }
+
+  test("can handle empty price array") {
+    val prices = Array.empty[Int]
+    val result = OnlineStockSpan.calulcateSpan(prices)
+    result shouldEqual (Array.empty[Int])
+  }
+
+  test("can handle array with 1 element") {
+    val prices = Array(10)
+    val result = OnlineStockSpan.calulcateSpan(prices)
+    result shouldEqual (Array(0))
   }
 }
