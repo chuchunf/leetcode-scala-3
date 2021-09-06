b402dc553c5cf620ed386962a502599041ee2bb3
chuchunf <chuchunf@users.noreply.github.com>
2021-09-06 11:09:54 +0800

    add add Compare Version Numbers

---
+++ src/main/scala/string/CompareVersionNumbers.scala
@@ -0,0 +1,17 @@
+package string
+
+object CompareVersionNumbers {
+  def compareVersion(version1: String, version2: String): Int =
+    val (vs1, vs2) = (version1.split("\\."), version2.split("\\."))
+    val maxparts = vs1.length.max(vs2.length)
+    for (i <- 0 until maxparts) {
+      val v1v = if i >= vs1.length then 0 else getVersion(vs1(i))
+      val v2v = if i >= vs2.length then 0 else getVersion(vs2(i))
+      if v1v != v2v then return v1v.compare(v2v)
+    }
+    0
+
+  private def getVersion(v: String): Int = v.indexWhere(c => c.isDigit && !c.isSpaceChar) match
+    case len if len >= v.length => 0
+    case len => v.substring(len).toInt
+}
---
+++ src/test/scala/string/CompareVersionNumbersTest.scala
@@ -0,0 +1,27 @@
+package string
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+class CompareVersionNumbersTest extends AnyFunSuite {
+  test("can compare the same") {
+    val v1 = "1.01"
+    val v2 = "1.01"
+    val result = CompareVersionNumbers.compareVersion(v1, v2)
+    result shouldEqual (0)
+  }
+
+  test("can compare and ingore leading zero") {
+    val v1 = "1.01"
+    val v2 = "1.1"
+    val result = CompareVersionNumbers.compareVersion(v1, v2)
+    result shouldEqual (0)
+  }
+
+  test("can find diff in minor version") {
+    val v1 = "2.01"
+    val v2 = "2.2"
+    val result = CompareVersionNumbers.compareVersion(v1, v2)
+    result shouldEqual (-1)
+  }
+}
