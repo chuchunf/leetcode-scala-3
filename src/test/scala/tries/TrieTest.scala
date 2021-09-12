b86ccc5db25436cee7b5be2dc5e0717351ee53a4
chuchunf <chuchunf@users.noreply.github.com>
2021-09-12 12:35:04 +0800

    update tries testing cases

--- src/test/scala/tries/TrieTest.scala
+++ src/test/scala/tries/TrieTest.scala
@@ -10,13 +10,31 @@ class TrieTest extends AnyFunSuite {
     trie.insert("am", 20)
     trie.insert("a", 30)
     trie.insert("trie", 40)
-    println(s"trie=$trie")
+    trie.toString shouldEqual (
+      """[ ]
+        |+    [a](30)
+        ||    +    [m](20)
+        |\   [i](10)
+        |\   [t]
+        |    +    [r]
+        |    |    +    [i]
+        |    |    |    +    [e](40)
+        |""".stripMargin)
 
     var result = trie.search("i")
     result shouldEqual (Some(10))
 
     trie.delete("am")
-    println(s"$trie")
+    trie.toString shouldEqual (
+      """[ ]
+        |+    [a](30)
+        ||    +    [m]
+        |\   [i](10)
+        |\   [t]
+        |    +    [r]
+        |    |    +    [i]
+        |    |    |    +    [e](40)
+        |""".stripMargin)
 
     result = trie.search("am")
     result shouldEqual (None)
