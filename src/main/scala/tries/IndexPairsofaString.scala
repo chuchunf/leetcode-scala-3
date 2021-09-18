139ad32c01b97fd83340817e7e49ae376e9aeed0
chuchunf <chuchunf@users.noreply.github.com>
2021-09-18 11:04:26 +0800

    add Index Pairs of a String

---
+++ src/main/scala/tries/IndexPairsofaString.scala
@@ -0,0 +1,17 @@
+package tries
+
+import scala.collection.mutable
+
+object IndexPairsofaString {
+  def indexPairs(s: String, words: List[String]): List[List[Int]] =
+    val (result, trie) = (mutable.ListBuffer[List[Int]](), Trie.empty[Int])
+    words.foreach(word => trie.insert(word, word.length))
+    for (i <- 0 until s.length) indexPairsInternal(s, i, 0, trie, result)
+    result.toList
+
+  private def indexPairsInternal(s: String, start: Int, step: Int, trie: Trie[Int], result: mutable.ListBuffer[List[Int]]): Unit =
+    trie.children(s.charAt(start + step) - 97) match
+      case Some(node) => if node.value.isDefined then result.addOne(List(start, start + step))
+        if start + step < s.length then indexPairsInternal(s, start, step + 1, node, result)
+      case _ =>
+}
---
+++ src/test/scala/tries/IndexPairsofaStringTest.scala
@@ -0,0 +1,13 @@
+package tries
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+
+class IndexPairsofaStringTest extends AnyFunSuite {
+  test("can index pairs of a string") {
+    val s = "thestoryofleetcodeandme"
+    val words = List("story","fleet","leetcode")
+    val result = IndexPairsofaString.indexPairs(s, words)
+    result shouldEqual (List(List(3, 7), List(9, 13), List(10, 17)))
+  }
+}
