fdf25e7e5a104f3fc91d9af9c66772318d38396a
chuchunf <chuchunf@users.noreply.github.com>
2021-09-25 13:27:37 +0800

    add Int to Roman

---
+++ src/main/scala/math/math.md
@@ -0,0 +1,10 @@
+# Math
+
+### 12. Integer to Roman
+```scala
+  def intToRoman(num: Int): String =
+    List((1000, "M"), (900, "CM"), (500, "D"), (400, "CD"), (100, "C"), (90, "XC"), (50, "L"), (40, "XL"), (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I"))
+      .find { case (int, _) => num >= int }
+      .map { case (int, roman) => if num == int then roman else roman + intToRoman(num - int) }
+      .get
+```
---
+++ src/test/scala/math/Integer2RomanTest.scala
@@ -0,0 +1,18 @@
+package math
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers._
+
+class Integer2RomanTest extends AnyFunSuite {
+  test("can convert 1994") {
+    val num = 1994
+    val result = Integer2Roman.intToRoman(num)
+    result shouldEqual ("MCMXCIV")
+  }
+
+  test("can convert 3") {
+    val num = 3
+    val result = Integer2Roman.intToRoman(num)
+    result shouldEqual ("III")
+  }
+}
