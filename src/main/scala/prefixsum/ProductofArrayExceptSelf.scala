db590c94d512d5ac6b4fe814228b8849604e829f
chuchunf <chuchunf@users.noreply.github.com>
2021-10-24 16:09:24 +0800

    misc updates

--- src/main/scala/prefixsum/ProductofArrayExceptSelf.scala
+++ src/main/scala/prefixsum/ProductofArrayExceptSelf.scala
@@ -4,8 +4,8 @@ object ProductofArrayExceptSelf {
   def productExceptSelf(nums: Array[Int]): Array[Int] =
     val result = Array.fill[Int](nums.length)(1)
     nums.drop(1).zipWithIndex.foldLeft(nums.head) { case (prod, (num, index)) =>
-      result(1 + index) = prod
-      result(1 + index) * num
+      result(index + 1) = prod
+      prod * num
     }
     nums.dropRight(1).zipWithIndex.foldRight(nums.last) { case ((num, index), prod) =>
       result(index) = prod * result(index)
