bee951671ab0e6a09f2f75af548af887e9ef9364
chuchunf <chuchunf@users.noreply.github.com>
2021-10-30 08:24:20 +0800

    add Continuous Subarray Sum

---
+++ src/main/scala/prefixsum/ContinuousSubarraySum.scala
@@ -0,0 +1,13 @@
+package prefixsum
+
+import scala.collection.mutable
+
+object ContinuousSubarraySum {
+  def checkSubarraySum(nums: Array[Int], k: Int): Boolean =
+    val cache = mutable.HashSet[Int]()
+    var sum = 0
+    nums.find { num =>
+      sum = sum + num
+      !cache.add(sum % k)
+    }.isDefined
+}
