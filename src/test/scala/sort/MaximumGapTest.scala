c8c92d0c3899f0ec27dca97289020145bc19162d
chuchunf <chuchunf@users.noreply.github.com>
2021-11-27 12:48:42 +0800

    add Maximum Gap

---
+++ src/test/scala/sort/MaximumGapTest.scala
@@ -0,0 +1,13 @@
+package sort
+
+import linkedlist.LinkedListNode
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.*
+
+class MaximumGapTest extends AnyFunSuite {
+  test("can find max gap") {
+    val nums = Array(3, 6, 9, 1)
+    val result = MaximumGap.maximumGap(nums)
+    result shouldEqual (3)
+  }
+}
