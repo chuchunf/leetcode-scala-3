3a431d1a59e22596618e764511692d3900b3ce48
chuchunf <chuchunf@users.noreply.github.com>
2022-07-16 14:08:13 +0800

    refactor Jump Game

--- src/main/scala/dp/JumpGame.scala
+++ src/main/scala/dp/JumpGame.scala
@@ -1,14 +1,13 @@
 package dp
 
 object JumpGame {
-  def canJump(nums: Array[Int]): Boolean = _canJump(0, nums.length, nums, Array.ofDim[Option[Boolean]](nums.length))
+  def canJump(nums: Array[Int]): Boolean = _canJump(0, nums.length, nums, Array.fill[Option[Boolean]](nums.length)(None))
 
   def _canJump(current: Int, n: Int, nums: Array[Int], cache: Array[Option[Boolean]]): Boolean =
-    if cache(current) != null then cache(current).get
+    if cache(current).nonEmpty then cache(current).get
     else if nums(current) + current > n then true
     else (1 to nums(current)).foldLeft(false) { case (result, i) =>
-      val iresult = _canJump(current + i, n, nums, cache)
-      cache(current + i) = Some(iresult)
-      result || iresult
+      cache(current + i) = Some(_canJump(current + i, n, nums, cache))
+      result || cache(current + i).get
     }
 }
