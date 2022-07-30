8ce86f90f9bb24597e2de9b4794e89cc6f5b0105
chuchunf <chuchunf@users.noreply.github.com>
2022-07-30 15:56:17 +0800

    refactor Course Schedule, use hashset instead of list

--- src/main/scala/graph/CourseSchedule.scala
+++ src/main/scala/graph/CourseSchedule.scala
@@ -7,9 +7,9 @@ object CourseSchedule {
     val nodes = new Array[Node[Int]](numCourses)
     for (i <- 0 until numCourses) nodes(i) = Node(i)
     prerequisites.foreach(p => nodes(p(0)).neighbors.addOne(nodes(p(1))))
-    nodes.find(_canFinish(_, mutable.ListBuffer[Int]())).isEmpty
+    nodes.find(_canFinish(_, mutable.HashSet[Int]())).isEmpty
 
-  private def _canFinish(node: Node[Int], visited: mutable.ListBuffer[Int]): Boolean =
+  private def _canFinish(node: Node[Int], visited: mutable.HashSet[Int]): Boolean =
     visited.contains(node.value) || {
       visited.addOne(node.value)
       node.neighbors.find(_canFinish(_, visited)).nonEmpty
