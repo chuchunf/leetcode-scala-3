0d9751ef0631ffbd69dd7b82b7aa3f9f6f9264ee
chuchunf <chuchunf@users.noreply.github.com>
2022-09-03 23:15:46 +0800

    fix a bug in container with most water

--- src/main/scala/twopointers/ContainerWithMostWater.scala
+++ src/main/scala/twopointers/ContainerWithMostWater.scala
@@ -4,7 +4,7 @@ object ContainerWithMostWater {
   def maxArea(height: Array[Int]): Int =
     var (max, right, left) = (0, 0, height.length - 1)
     while right < left do
-      max = max.max(height(right) * height(left))
+      max = max.max(height(right).min(height(left)) * (left - right))
       if height(right) > height(left) then left = left - 1
       else right = right + 1
     max
--- src/test/scala/twopointers/ContainerWithMostWaterTest.scala
+++ src/test/scala/twopointers/ContainerWithMostWaterTest.scala
@@ -1,24 +1,30 @@
 package twopointers
 
 import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.*
 
 class ContainerWithMostWaterTest extends AnyFunSuite {
-  test("can handle mininium case") {
+  test("can handle minimum case") {
     val a = Array(1, 1)
     val result = ContainerWithMostWater.maxArea(a)
-    result shouldEqual (1)
+    result shouldEqual 1
   }
 
   test("can handle normal case") {
-    val a = Array(4,3,2,1,4)
+    val a = Array(4, 3, 2, 1, 4)
     val result = ContainerWithMostWater.maxArea(a)
-    result shouldEqual (16)
+    result shouldEqual 16
   }
 
   test("can handle normal case from right") {
-    val a = Array(4,1,2,3,4)
+    val a = Array(4, 1, 2, 3, 4)
     val result = ContainerWithMostWater.maxArea(a)
-    result shouldEqual (16)
+    result shouldEqual 16
+  }
+
+  test("can handle leetcode case") {
+    val a = Array(1, 8, 6, 2, 5, 4, 8, 3, 7)
+    val result = ContainerWithMostWater.maxArea(a)
+    result shouldEqual 49
   }
 }
