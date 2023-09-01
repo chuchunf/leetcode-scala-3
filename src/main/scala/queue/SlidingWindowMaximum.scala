befd3fd65f059341fa6fa3b691612adba6902bf2
chuchunf <chuchunf@users.noreply.github.com>
2023-09-01 14:17:15 +0800

    Update SlidingWindowMaximum.scala
    misc update

--- src/main/scala/queue/SlidingWindowMaximum.scala
+++ src/main/scala/queue/SlidingWindowMaximum.scala
@@ -4,10 +4,13 @@ import scala.collection.mutable
 
 object SlidingWindowMaximum {
   def maxSlidingWindow(nums: Array[Int], k: Int): Array[Int] =
-    nums.indices.foldLeft((mutable.ArrayDeque[Int](), mutable.ArrayBuffer[Int]())) { case ((q, solution), i) =>
-      q.removeLastWhile { last => nums(last) < nums(i) && last >= (i - k + 1) }
-      q.append(i)
-      q.removeHeadWhile(_ < i - k + 1)
-      if i >= (k - 1) then (q, solution :+ nums(q.head)) else (q, solution)
+    // fold/slide the windows of k from left of the array
+    nums.indices.foldLeft((mutable.ArrayDeque[Int](), mutable.ArrayBuffer[Int]())) {
+      // q: a double queue to hold the current, solution: array to store final result, i, the current index
+      case ((q, solution), i) =>
+        q.removeLastWhile { last => nums(last) < nums(i) && last >= (i - k + 1) }
+        q.append(i)
+        q.removeHeadWhile(_ < i - k + 1)
+        if i >= (k - 1) then (q, solution :+ nums(q.head)) else (q, solution)
     }._2.toArray
 }
